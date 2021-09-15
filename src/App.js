742c8ff059489d75fa232b9bf072c7d75cee5112
meldsilva <meldsilva@gmail.com>
2021-09-14 23:20:14 -0400

    Working version of react router tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {Home, About, Events, Contacts, Whoops404, Services, CompanyHistory, Location} from "./pages";
 
 function App() {
         return <div>
-            <h1>Hello React Testing Library</h1>
+            <Routes>
+                <Route  path="/" element={<Home />} />
+
+                <Route  path="/about" element={<About />}>
+
+                    <Route  path="/services" element={<Services />} />
+                    <Route  path="/history" element={<CompanyHistory />} />
+                    <Route  path="/location" element={<Location />} />
+                    </Route>
+                <Route  path="/events" element={<Events />} />
+                <Route  path="/contacts" element={<Contacts />} />
+                <Route  path="*" element={<Whoops404 />} />
+            </Routes>
         </div>
 }
-
 export default App;
--- src/Checkbox.test.js
+++ src/Checkbox.test.js
@@ -1,5 +1,3 @@
-// import { fireEvent } from "@testing-library/dom";
-// import { render } from "react-dom"
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import {Checkbox} from "./Checkbox"
